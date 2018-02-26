ba040ca12a5693c8fb48c61dd5ebaf7f21f8c051
ywl <1726423716@qq.com>
2018-02-26 20:07:00 +0800

    更新登录页静态页

---
+++ my-project/public/javascripts/SCshop.js
@@ -0,0 +1,29 @@
+window.onload = function(){
+	yzm();
+}
+
+$(".random").click(function(){
+	yzm();
+})
+
+function yzm(){
+	var arr = [1,2,3,4,5,6,7,8,9,0,"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+	var str = "";
+	for( var i = 0; i <4; i++ ){
+		str += arr[Math.floor(Math.random()*36)];
+	}	
+	$(".random").html(str);
+	return str;
+
+}
+
+$("#yzm").blur(function(){
+	var str = $(".random").html();
+	if( $(this).val()=="" ){
+		alert("您还没有输入验证码！")
+	}else{
+		if( $("#yzm").val() != str ){
+			alert("请填写正确的验证码！")
+		}
+	}
+})
