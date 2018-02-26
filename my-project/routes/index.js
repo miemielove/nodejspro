4493aaf6a835ee971d20396d87e37b349362ea68
ywl <1726423716@qq.com>
2018-02-26 23:02:39 +0800

    “一天

--- my-project/routes/index.js
+++ my-project/routes/index.js
@@ -3,11 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'ECSHOP管理中心' });
 });
 
 router.get('/login', function(req, res, next) {
   res.render('login', { title: '登录页面' });
 });
+
+router.get('/index/index-head', function(req, res, next) {
+  res.render('index-head', { title: 'ECSHOP管理中心' });
+});
+
 
 module.exports = router;
